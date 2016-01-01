8e9bf86784991bd709712343a3ec7407ea7d25ab
Roman Usherenko <roman.usherenko@gmail.com>
2016-01-01 23:25:22 +0000

    do not set user db for device controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  before_action :select_sqlite_db
+  before_action :select_sqlite_db, unless: :devise_controller?
 
   private
 

cdc85a374317cf3f48e66d49aab2b61d758ad79b
Roman Usherenko <roman.usherenko@gmail.com>
2018-04-23 13:14:15 +0300

    use determenistic name for db in dev

--- app/controllers/databases_controller.rb
+++ app/controllers/databases_controller.rb
@@ -4,9 +4,15 @@ class DatabasesController < ApplicationController
   def create
     render :new_db and return unless (file = params[:upload][:file]).present?
 
-    current_user.database = Database.new(file: file.tempfile)
+    current_user.database = Database.new(file: file.tempfile, name: database_name)
     current_user.save
 
     redirect_to accounts_path
+  end
+
+  private
+
+  def database_name
+    Rails.env.development? ? current_user.id : nil
   end
 end
--- app/models/database.rb
+++ app/models/database.rb
@@ -1,20 +1,25 @@
 class Database < ActiveRecord::Base
   after_destroy :remove_file
+  after_create :save_file
 
-  def file=(uploaded_file)
-    self.name ||= SecureRandom.hex(4)
-    FileUtils.cp(uploaded_file, file_path)
-  end
+  attr_accessor :file
 
   def file_path
     folder = ENV['OPENSHIFT_DATA_DIR'] || Rails.root.join('db')
-    "#{folder}/#{name}.sqlite3"
+    "#{folder}/#{name || SecureRandom.hex(4)}.sqlite3"
   end
 
   private
 
+  def save_file
+    file or return
+
+    FileUtils.cp(file, file_path)
+  end
+
   def remove_file
     FileUtils.rm file_path
   rescue Errno::ENOENT
+    nil
   end
 end

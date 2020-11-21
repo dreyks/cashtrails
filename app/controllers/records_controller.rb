6d276f860a3cd1f52da4a97699c69e5f06f2cab6
Roman Usherenko <roman.usherenko@gmail.com>
2020-11-21 22:17:54 +0200

    filter records by account

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,6 +1,16 @@
 class RecordsController < ApplicationController
   def index
-    @records = Record.order(gmtDate: :desc, gmtTime: :desc).page params[:page]
+    @records = Record.all
+
+    if params[:account_id]
+      @record = @records.
+          where(account1IDOrInvalid: params[:account_id]).
+          or(Record.where(account2IDOrInvalid: params[:account_id]))
+    end
+
+    @records = @records.
+        order(gmtDate: :desc, gmtTime: :desc).
+        page params[:page]
   end
 
   def edit
